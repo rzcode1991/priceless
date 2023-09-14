d54e37d642d4082784db5682c014a7e70de93401
Codei1991 <rzslmn90@gmail.com>
2023-09-14 19:08:06 +0430

    Your commit message here

---
+++ app/src/main/java/com/example/priceless/FullScreenPostImageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.priceless
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class FullScreenPostImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_post_image)
+
+        if (intent.hasExtra("post_image")){
+            val postImage = intent.getStringExtra("post_image")
+            val ivFullScreenPostImage: ImageView = findViewById(R.id.iv_full_screen_post_image)
+            if (postImage != null){
+                GlideLoader(this).loadImageUri(postImage, ivFullScreenPostImage)
+            }
+        }
+    }
+}
